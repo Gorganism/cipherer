11478be961b43b968f45d90664290955fca0c50c
Gorganism <gogium@proton.me>
2026-01-15 14:13:05 -0600

    added basic stuff for later
    very basic things.

--- cipherer.py
+++ cipherer.py
@@ -1,1 +1,12 @@
+# from PySide6.QtWidgets import (
+#     QApplication, QWidget, QVBoxLayout, QLabel, QTextEdit, QPushButton
+# )
+# from PySide6.QtGui import QTextCursor
+# from PySide6.QtCore import Qt, QEvent, QObject
 
+# TODO - backend and allat
+
+# class Interface(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         # TODO - stuff here for UI
