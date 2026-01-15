0c2571ce4f395c3365f65ccd8751c53ea16d8e59
Gorganism <gogium@proton.me>
2026-01-15 14:29:55 -0600

    small update
    added test key and rand

--- cipherer.py
+++ cipherer.py
@@ -1,10 +1,28 @@
+import random as rand
 # from PySide6.QtWidgets import (
 #     QApplication, QWidget, QVBoxLayout, QLabel, QTextEdit, QPushButton
 # )
 # from PySide6.QtGui import QTextCursor
 # from PySide6.QtCore import Qt, QEvent, QObject
 
-# TODO - backend and allat
+# --- BACKEND STUFFS ---
+
+alphabet = ['a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y',
+            'z']
+
+# temporary key for testing
+testkey = ['c', 'l', 't', 'h', 'a',
+           'n', 'v', 'w', 'z', 'y',
+           's', 'k', 'r', 'p', 'm',
+           'o', 'd', 'i', 'x', 'f',
+           'b', 'j', 'e', 'g', 'u',
+           'q']
+
+# TODO - finish backend and allat
 
 # class Interface(QWidget):
 #     def __init__(self):
