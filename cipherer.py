534bd9b224a506129b8f5e603e9c02bc9a71351c
Bananaguy20 <167466281+Bananaguy20@users.noreply.github.com>
2026-01-23 12:45:41 -0600

    Update cipherer.py

--- cipherer.py
+++ cipherer.py
@@ -1,3 +1,111 @@
+import random as rand
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QLabel, QTextEdit, QPushButton, QCheckBox
+)
+from PySide6.QtGui import QTextCursor
+from PySide6.QtCore import Qt, QEvent, QObject
+import sys
+
+# --- BACKEND STUFFS ---
+
+# temporary key for testing. it's SUPPOSED to be uppercase. do not edit!
+testkey = [('a','C'), ('b','L'), ('c','T'), ('d','H'), ('e','A'),
+           ('f','N'), ('g','V'), ('h','W'), ('i','Z'), ('j','Y'),
+           ('k','S'), ('l','K'), ('m','R'), ('n','P'), ('o','M'),
+           ('p','O'), ('q','D'), ('r','I'), ('s','X'), ('t','F'),
+           ('u','B'), ('v','J'), ('w','E'), ('x','G'), ('y','U'),
+           ('z','Q')]
+key=testkey
+
+def spacer(h, p): # adds a space at a location in a string. this exists solely for legibility.
+    return h[:p] + " " + h[p:]
+
+def encode(key, entry, patristocrat):
+    result = entry
+    for (i,j) in key:
+       result = result.replace(i,j)
+    result = result.lower()
+    if patristocrat:
+        result = result.replace(" ","")
+        # for y in range(5, len(result)+6, 6):
+        for y in range(5, len(result)*2, 6):
+            result = spacer(result,y)
+    return result.strip()
+
+
+def decrypt(key,encrypted): # TODO change name of epic variable
+    result=[]
+    encrypted=encrypted.upper()
+    encrypted=list(encrypted)
+    for i in range(len(encrypted)):
+        p=-1
+        epic="hi"
+        while epic != encrypted[i]:
+            p+=1
+            epic=key[p][1]
+            if encrypted[i]==" ":
+                result.append(" ")
+                break
+        if encrypted[i]==" ":
+            print("added space to the list")
+        else:
+            result.append(key[p][0])
+    return("".join(result))
+
+# TODO - finish backend and allat
+
+
+# gorg's version of the decoder tool:
+
+# def decode(key, entry, patristocrat):
+#     result = entry
+#     for (i,j) in key:
+#        result = result.replace(j.lower(),i.upper())
+#     result = result.lower()
+#     if patristocrat:
+#         result = result.replace(" ","")
+#     return result
+
+
+# --- TESTING ---
+
+patr_toggle = 0 # patristocrat toggle; change to a toggle in the GUI later
+
+# print(encode(testkey,input("Phrase to encode: "),patr_toggle)) # encoder call
+
+# print(decode(testkey,input("Phrase to decode: "),patr_toggle)) # gorg decoder call
+
+# alphabet = set('abcdefghijklmnopqrstuvwxyz')
+# userkey = input("Please input your key: ") # in the GUI later, note that the case is not preserved.
+# while not (len(userkey) == 26 and alphabet.issubset(key)):
+#     userkey = input("The key was the wrong length or it was missing characters please input a new key: ")
+
+# --- FRONTEND / UI ---
+
+class Cipherer(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Cipherer")
+        self.rounded_box_style = """
+            background-color: #181825;
+            color: #a6adc8;
+            font-size: 15pt;
+            border-radius: 0.5em;
+            padding: 0.5em;
+        """
+        self.button_style = """
+            background-color: #f2cdcd;
+            color: #1e1e2e;
+            font-weight: bold;
+            font-size: 15pt;
+            border-radius: 0.5em;
+            padding: 0.5em;
+        """
+        self.textbox_headers = """
+            font-weight: bold;
+            font-size: 20pt;
+        """
+        
         # layout
         self.layout = QVBoxLayout(self)
 
@@ -117,8 +225,3 @@ if __name__ == "__main__":
     window = Cipherer()
     window.setStyleSheet("""
         color: #cdd6f4;
-        background-color: #1e1e2e;
-    """)
-    window.resize(500, 600)
-    window.show()
-    sys.exit(app.exec())
